4ae8d3bdf5cb47ca1b5721c79150a19b557d0f7e
Pirogov Evgenij <pirogovevgenij@gmail.com>
2020-08-16 19:50:21 +0300

    initial commit

---
+++ build/precache-manifest.47a11514fa85e2792f552fb2dc080d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfe78cb878afed774a2f3c854b8af36",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba2066f280d23a6f3613",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5134f1f495428e35dc92",
+    "url": "./static/js/2.fbcfa010.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fbcfa010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2066f280d23a6f3613",
+    "url": "./static/js/main.4253dc24.chunk.js"
+  },
+  {
+    "revision": "ecb9b274f6186eb135ab",
+    "url": "./static/js/runtime-main.0708872f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
